9f9451f0f7c5f426ce0e5b6bf595cd3a23da8782
화베(조유찬) <81742754+whdbcks111@users.noreply.github.com>
2023-07-04 18:39:17 +0900

    Stat and Effect logic complete

--- Assets/01.Scripts/StatusEffects/StatusEffect.cs
+++ Assets/01.Scripts/StatusEffects/StatusEffect.cs
@@ -6,15 +6,17 @@ public abstract class StatusEffect
 {
     public int Level;
     public float Duration;
+    public Entity Caster { get; private set; }
     public float MaxDuration { get; private set; }
 
-    public StatusEffect(int level, float duration)
+    public StatusEffect(int level, float duration, Entity caster = null)
     {
         Level = level;
         MaxDuration = Duration = duration;
+        Caster = caster;
     }
 
-    public abstract void OnStart();
-    public abstract void OnUpdate();
-    public abstract void OnFinish();
+    public abstract void OnStart(Entity target);
+    public abstract void OnUpdate(Entity target);
+    public abstract void OnFinish(Entity target);
 }
