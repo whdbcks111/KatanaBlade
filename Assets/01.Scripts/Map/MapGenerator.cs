352101166249f6959b231c0b65a247c29354b238
화베(조유찬) <81742754+whdbcks111@users.noreply.github.com>
2023-07-06 10:45:29 +0900

    Parrying feedback stat added

--- Assets/01.Scripts/Map/MapGenerator.cs
+++ Assets/01.Scripts/Map/MapGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,13 @@ using UnityEngine.Tilemaps;
 public class MapGenerator : MonoBehaviour
 {
     public static readonly int MapSize = 30, StageCount = 50, BossCount = 3;
-    public static readonly Vector2Int[] Directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
     public static MapGenerator Instance;
 
     [SerializeField] Tilemap _targetTilemap;
+    [SerializeField] StageShape _spawnShape;
     [SerializeField] StageShape[] _shapes;
+
+    private readonly Dictionary<Vector2Int, StageShape> _map = new();
 
     private void Awake()
     {
@@ -26,16 +29,41 @@ public class MapGenerator : MonoBehaviour
     {
         _targetTilemap.ClearAllTiles();
 
-        
+        CreateMapTiles(Vector2Int.zero, _spawnShape);
+        TryGenerateTransition(Vector2Int.zero);
     }
 
-    public void TryGenerateTransition()
+    public void TryGenerateTransition(Vector2Int curPos, int transitionCount = 0)
     {
+        var curShape = _map[curPos];
+        if (curShape is null) return;
 
+        List<Vector2Int> allowDirections = new();
+        if (curShape.IsTopOpened) allowDirections.Add(Vector2Int.up);
+        if (curShape.IsBottomOpened) allowDirections.Add(Vector2Int.down);
+        if (curShape.IsLeftOpened) allowDirections.Add(Vector2Int.left);
+        if (curShape.IsRightOpened) allowDirections.Add(Vector2Int.right);
+    }
+
+    public void CreateMapTiles(Vector2Int pos, StageShape shape)
+    {
+        _map[pos] = shape;
+
+        Vector3Int mapCenterPos = (Vector3Int)pos * MapSize;
+
+        for(int i = -MapSize / 2; i < MapSize / 2; ++i)
+        {
+            for (int j = -MapSize / 2; j < MapSize / 2; ++j)
+            {
+                _targetTilemap.SetTile(mapCenterPos + Vector3Int.right * i + Vector3Int.up * j,
+                    shape.ShapeMap.GetTile(Vector3Int.right * i + Vector3Int.up * j));
+            }
+        }
     }
 }
 
-public struct StageShape
+[Serializable]
+public class StageShape
 {
     public Tilemap ShapeMap;
     public Transform KeyPositions;
