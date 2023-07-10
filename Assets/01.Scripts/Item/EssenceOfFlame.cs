6edb93f96f14ce80a1e33d372202a7ed1a845c90
화베(조유찬) <81742754+whdbcks111@users.noreply.github.com>
2023-07-10 15:17:09 +0900

    Item class abstract method fixed

--- Assets/01.Scripts/Item/EssenceOfFlame.cs
+++ Assets/01.Scripts/Item/EssenceOfFlame.cs
@@ -63,4 +63,14 @@ public class EssenceOfFlame : Item
             _dT = 0;
         }
     }
+
+    public override void OnMount()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUnmount()
+    {
+        throw new System.NotImplementedException();
+    }
 }
