32f64facfd5627a5a02c78b95efe006bb2c9b45a
superman2080 <superman2080@naver.com>
2023-07-10 18:19:23 +0900

    ssss

--- Assets/01.Scripts/Item/EssenceOfFlame.cs
+++ Assets/01.Scripts/Item/EssenceOfFlame.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class EssenceOfFlame : Item
 {
-    public float ActiveRadius = 10;
+    public float ActiveRadius = 15;
     private static readonly float ActiveDamage = 5f;
 
     public float PassiveTick;
@@ -24,13 +25,14 @@ public class EssenceOfFlame : Item
     {
     }
 
+    [ContextMenu("액티브 사용")]
     public override void OnActiveUse()
     {
         if (_lastUsed > 0 && (Time.realtimeSinceStartup - _lastUsed) < Cooldown) return;
         _lastUsed = Time.realtimeSinceStartup;
 
         //적 레이어 추가해야함
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(Player.Instance.transform.position, PassiveRadius);
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(Player.Instance.transform.position, ActiveRadius);
         
         if(enemies.Length > 0)
         {
@@ -42,7 +44,10 @@ public class EssenceOfFlame : Item
                     minDist = i;
                 }
             }
-
+            GameObject gO = new GameObject();
+            gO.transform.position = Player.Instance.transform.position;
+            Tilemap map = GameObject.Find("Tilemap").GetComponent<Tilemap>();
+            StartCoroutine(ChaseTarget(map, gO.transform, enemies[minDist].transform.position, 1f));
             //적에게 투사체 발사 코드, 맞은 적에게 대미지 입히는 코드
         }
     }
@@ -62,5 +67,38 @@ public class EssenceOfFlame : Item
             }
             _dT = 0;
         }
+    }
+    
+    private IEnumerator ChaseTarget(Tilemap tilemap, Transform obj, Vector2 target, float totalTime)
+    {
+        while ((Vector2)obj.transform.position != target)
+        {
+            var position = obj.transform.position;
+            var nextPos = Pathfinder.GetNextPath(tilemap, position, target);
+            position = Vector2.MoveTowards(position, nextPos, 5 * Time.deltaTime);
+            obj.transform.position = position;
+            Debug.Log(position);
+            //dT += Time.deltaTime;
+            yield return null;
+        }
+        //int cnt = path.Count;
+        //Debug.Log(cnt);
+        //float dT = 0;
+        //while(path.Count > 0)
+        //{
+        //    Path dir = path.Pop();
+        //    Vector2 originPos = obj.transform.position;
+        //    while (true)
+        //    {
+        //        if((Vector2)obj.transform.position == originPos + dir.Pos)
+        //        {
+        //            break;
+        //        }
+
+        //        obj.transform.position = Vector2.Lerp(originPos, originPos + dir.Pos, dT / (totalTime / cnt));
+        //        dT += Time.deltaTime;
+        //        yield return null;
+        //    }
+        //}
     }
 }
