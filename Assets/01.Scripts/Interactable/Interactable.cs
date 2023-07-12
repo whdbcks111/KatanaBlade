13c17c709fed0c6ed80028427844a68a8a213f27
화베(조유찬) <81742754+whdbcks111@users.noreply.github.com>
2023-07-12 14:42:43 +0900

    Parrying bug fixed

--- Assets/01.Scripts/Interactable/Interactable.cs
+++ Assets/01.Scripts/Interactable/Interactable.cs
@@ -6,6 +6,14 @@ public abstract class Interactable : MonoBehaviour
 {
     [SerializeField] private float _interactDistance = 2;
 
+    private void Awake()
+    {
+        var hint = Instantiate(Resources.Load<Canvas>("UI/InteractHint"));
+        hint.transform.SetParent(transform);
+        hint.transform.localPosition = Vector3.zero;
+        hint.GetComponentInChildren<InteractHint>().InteractDistance = _interactDistance;
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.W) && 
