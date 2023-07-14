b2f1ea5412d6a3c896c1d2b7d7538bc1eba9960f
redflag-HSH <dianinba2@gmail.com>
2023-07-14 18:40:59 +0900

    Boy Boss skul added
    Boss script is now adjusted for common feature use of bosses
    Wallcheck for ex.
    boy boss' skul boss is now workable.
    boy boss is WIP

--- Assets/01.Scripts/Entity/Boss/Boss.cs
+++ Assets/01.Scripts/Entity/Boss/Boss.cs
@@ -4,5 +4,53 @@ using UnityEngine;
 
 public class Boss : Monster
 {
+    public bool IsActable;
+    protected float _distance;
+    protected int _aiMode;
+    protected Player _player;
     public bool IsAttcking;
+    protected int _stare;
+    protected Collider2D _col2d;
+    protected virtual void Start()
+    {
+        _player = Player.Instance;
+        _col2d = GetComponent<Collider2D>();
+    }
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if (IsActable)
+            AIAct();
+    }
+    protected void Staring()
+    {
+        if (_player.transform.position.x < transform.position.x)
+        {
+            _stare = -1;
+            //_renderer.flipX = false;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        }
+        else
+        {
+            _stare = 1;
+            //_renderer.flipX = true;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        //print(_stare);
+    }
+    protected void WallCheck()
+    {
+        if (MovingVelocity == 0)
+            return;
+
+        Debug.DrawRay(transform.position, new Vector2(MovingVelocity, 0), Color.blue);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(MovingVelocity, 0), _col2d.bounds.size.x / 2 + MovingVelocity, LayerMask.GetMask("Platform"));
+        if (hit.collider != null)
+            MovingVelocity = 0;
+    }
+    public virtual void AIAct()
+    {
+
+    }
 }
