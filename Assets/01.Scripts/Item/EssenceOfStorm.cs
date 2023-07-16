eaa5d692f1efab866f6c54bf594981b51f15c7c0
superman2080 <superman2080@naver.com>
2023-07-17 02:10:16 +0900

    daily commit

--- Assets/01.Scripts/Item/EssenceOfStorm.cs
+++ Assets/01.Scripts/Item/EssenceOfStorm.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class EssenceOfStorm : Item
 {
-    private static readonly float Cooldown = 5f;
+    private static readonly float Cooldown = 10f;
 
     private static readonly float MaintainTime = 2;
-    private static readonly float ActivePower = 2.5f;
-    private static readonly float SkillWidth = 7.5f;
+    private static readonly float ActivePower = 15f;
+    private static readonly float SkillWidth = 10f;
 
     public EssenceOfStorm()
 : base(ItemType.Essence, "폭풍의 정수",
     string.Format(
         "사용 시 : 주변에 폭풍을 일으켜 적들을 {0}초간 <color=skyblue>띄우고</color> 기절시킵니다. <color=gray>(재사용 대시기간 : {1:0.0}초)</color>\n" +
         "기본 지속 효과 : - ", MaintainTime,Cooldown),
-    Resources.Load<Sprite>("Item/Icon/Essence/Essence_0"))
+    Resources.Load<Sprite>("Item/Icon/Essence/Essence_9"))
     {
     }
 
@@ -43,7 +43,7 @@ public class EssenceOfStorm : Item
         float totalTime = 0;
         while(totalTime < maintainTime)
         {
-            Collider2D[] area = Physics2D.OverlapBoxAll(Player.Instance.transform.position, new Vector2(SkillWidth * 2, 5f), 0f, 1 << LayerMask.NameToLayer("Enemy"));
+            Collider2D[] area = Physics2D.OverlapCircleAll(Player.Instance.transform.position, SkillWidth * 2, 1 << LayerMask.NameToLayer("Enemy"));
             foreach (var enemy in area)
             {
                 if (enemy.GetComponent<Entity>() is Monster)
@@ -54,6 +54,7 @@ public class EssenceOfStorm : Item
                 }
             }
             totalTime += .1f;
+            Camera.main.GetComponent<CameraControll>().Shake(0.1f, Mathf.Clamp(totalTime, 0.1f, 1f));
             yield return new WaitForSeconds(0.1f);
         }
     }
